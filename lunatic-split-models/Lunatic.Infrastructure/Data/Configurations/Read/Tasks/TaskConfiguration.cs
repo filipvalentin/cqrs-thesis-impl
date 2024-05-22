c3fdd503b8ff8bce8dac7c5aad24e04b4c0ec30f
filipvalentin <valentin.filip@student.uaic.ro>
2024-05-22 22:13:53 +0300

    second model is working now, but at what cost?

--- lunatic-split-models/Lunatic.Infrastructure/Data/Configurations/Read/Tasks/TaskConfiguration.cs
+++ lunatic-split-models/Lunatic.Infrastructure/Data/Configurations/Read/Tasks/TaskConfiguration.cs
@@ -5,8 +5,23 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace Lunatic.Infrastructure.Data.Configurations.Read.Tasks {
 	internal sealed class TaskConfiguration : IEntityTypeConfiguration<TaskReadModel> {
 		public void Configure(EntityTypeBuilder<TaskReadModel> builder) {
-			builder.HasBaseType<BaseTaskReadModel>();
+			builder.ToTable("Tasks");
+
+			builder.HasKey(t => t.Id);
+			builder.Property(t => t.ProjectId).HasColumnName("ProjectId");
+			builder.Property(t => t.CreatedByUserId).HasColumnName("CreatedByUserId");
+			builder.Property(t => t.TaskSectionCard).HasColumnName("TaskSectionCard");
+			builder.Property(t => t.Title).HasColumnName("Title");
+			builder.Property(t => t.Description).HasColumnName("Description");
+			builder.Property(t => t.Priority).HasColumnName("Priority");
+			builder.Property(t => t.Status).HasColumnName("Status");
 			builder.Property(t => t.CommentIds).HasColumnName("CommentIds");
+			builder.Property(t => t.AssigneeIds).HasColumnName("AssigneeIds");
+			builder.Property(t => t.Tags).HasColumnName("Tags");
+			builder.Property(t => t.PlannedStartDate).HasColumnName("PlannedStartDate");
+			builder.Property(t => t.PlannedEndDate).HasColumnName("PlannedEndDate");
+			builder.Property(t => t.StartedDate).HasColumnName("StartedDate");
+			builder.Property(t => t.EndedDate).HasColumnName("EndedDate");
 		}
 	}
 }
